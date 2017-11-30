df55bd6585b19d7ad9357e2d3995bd36906a3917
Alexey Korolev <korolev2009@gmail.com>
2017-11-30 09:16:10 +0200

    Add AppDatabase (extends RoomDatabase)

---
+++ app/src/main/java/net/alexandroid/roomfirststepstutorial/db/AppDatabase.java
@@ -0,0 +1,22 @@
+package net.alexandroid.roomfirststepstutorial.db;
+
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
+
+@Database(entities = {Note.class}, version = 1)
+public abstract class AppDatabase extends RoomDatabase {
+
+    public static final String DATABASE_NAME = "notes";
+
+    private static AppDatabase INSTANCE;
+
+    public static AppDatabase getInstance(Context context) {
+        if (INSTANCE == null) {
+            INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                    AppDatabase.class, DATABASE_NAME).build();
+        }
+        return INSTANCE;
+    }
+}
