f22f5b1b3cefa304dbbe57a6e9276afd2b090b9a
Alexey Korolev <korolev2009@gmail.com>
2017-11-30 10:25:32 +0200

    Add RecyclerView with fake data

--- app/src/main/java/net/alexandroid/roomfirststepstutorial/MainActivity.java
+++ app/src/main/java/net/alexandroid/roomfirststepstutorial/MainActivity.java
@@ -1,13 +1,54 @@
 package net.alexandroid.roomfirststepstutorial;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
-public class MainActivity extends AppCompatActivity {
+import net.alexandroid.roomfirststepstutorial.db.Note;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements NotesAdapter.NotesAdapterInteraction {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setRecyclerView();
+    }
+
+    private void setRecyclerView() {
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        NotesAdapter notesAdapter = new NotesAdapter(this, getNoteList());
+        recyclerView.setAdapter(notesAdapter);
+    }
+
+    private List<Note> getNoteList() {
+        List<Note> list = new ArrayList<>();
+        list.add(createNote("Title 1 for tests", "Body 1 for tests"));
+        list.add(createNote("Title 2 for tests", "Body 2 for tests"));
+        list.add(createNote("Title 3 for tests", "Body 3 for tests"));
+        return list;
+    }
+
+    @NonNull
+    private Note createNote(String title, String body) {
+        Note note = new Note();
+        note.setTitle(title);
+        note.setBody(body);
+        return note;
+    }
+
+    // NotesAdapter.NotesAdapterInteraction interface methods
+    @Override
+    public void onDeleteNote(Note note) {
+
     }
 }
---
+++ app/src/main/java/net/alexandroid/roomfirststepstutorial/NotesAdapter.java
@@ -0,0 +1,69 @@
+package net.alexandroid.roomfirststepstutorial;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import net.alexandroid.roomfirststepstutorial.db.Note;
+
+import java.util.List;
+
+/**
+ * Created on 11/30/2017.
+ */
+
+public class NotesAdapter extends RecyclerView.Adapter<NotesAdapter.ViewHolder> implements View.OnClickListener {
+
+    private List<Note> mNoteList;
+    private final NotesAdapterInteraction mListener;
+
+    public NotesAdapter(NotesAdapterInteraction notesAdapterInteraction, List<Note> noteList) {
+        mListener = notesAdapterInteraction;
+        mNoteList = noteList;
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public final View mView;
+        public final TextView tvTitle;
+        public final TextView tvBody;
+
+        public ViewHolder(View view) {
+            super(view);
+            mView = view;
+            tvTitle = view.findViewById(R.id.tvTitle);
+            tvBody = view.findViewById(R.id.tvBody);
+        }
+    }
+
+    @Override
+    public NotesAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_note, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(NotesAdapter.ViewHolder holder, int position) {
+        holder.tvTitle.setText(mNoteList.get(position).getTitle());
+        holder.tvBody.setText(mNoteList.get(position).getBody());
+
+        holder.mView.setOnClickListener(this);
+        holder.mView.setTag(position);
+    }
+
+    @Override
+    public void onClick(View view) {
+        int position = (int) view.getTag();
+        mListener.onDeleteNote(mNoteList.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mNoteList.size();
+    }
+
+    public interface NotesAdapterInteraction {
+        void onDeleteNote(Note note);
+    }
+}
