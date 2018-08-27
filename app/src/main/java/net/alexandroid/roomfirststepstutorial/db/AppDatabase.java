8eb533969a3093d8b2bf7d5d06d5d09401c773a0
Anton Novikau <av.novikov@gmail.com>
2018-08-27 23:34:25 +0300

    Update libraries and migrate to jetpack

--- app/src/main/java/net/alexandroid/roomfirststepstutorial/db/AppDatabase.java
+++ app/src/main/java/net/alexandroid/roomfirststepstutorial/db/AppDatabase.java
@@ -1,9 +1,10 @@
 package net.alexandroid.roomfirststepstutorial.db;
 
-import android.arch.persistence.room.Database;
-import android.arch.persistence.room.Room;
-import android.arch.persistence.room.RoomDatabase;
 import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
 
 @Database(entities = {Note.class}, version = 1)
 public abstract class AppDatabase extends RoomDatabase {
