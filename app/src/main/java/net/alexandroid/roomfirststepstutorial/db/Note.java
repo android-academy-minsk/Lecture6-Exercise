054fbd0dfd0115fd3c73378a8cf8da8094b0f12a
Anton Novikau <av.novikov@gmail.com>
2018-08-27 23:34:25 +0300

    Update libraries and migrate to jetpack

--- app/src/main/java/net/alexandroid/roomfirststepstutorial/db/Note.java
+++ app/src/main/java/net/alexandroid/roomfirststepstutorial/db/Note.java
@@ -1,7 +1,7 @@
 package net.alexandroid.roomfirststepstutorial.db;
 
-import android.arch.persistence.room.Entity;
-import android.arch.persistence.room.PrimaryKey;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
 
 @Entity
 public class Note {
